a80239cc9608d45b0ef001718900fcbd88938496
unknown <2020a1r157@mietjammu.in>
2021-08-26 21:13:52 +0530

    Write a program to convert an infix expression to its corresponding to postfix and prefix expression

--- 05_Infix_Prefix.c
+++ 05_Infix_Prefix.c
@@ -1,130 +1,208 @@
-//Name:Ankush kumar
-//Roll No:2020A1r157
-//Program to converted infix to prefix
-# include <stdio.h>
-# include <string.h>
-# define MAX 20
-void infixtoprefix(char infix[20],char prefix[20]);
-void reverse(char array[30]);
-char pop();
-void push(char symbol);
-int isOperator(char symbol);
-int prcd(symbol);
-int top=-1;
-char stack[MAX];
-main() {
-  char infix[20],prefix[20],temp;
-  printf("Enter infix operation: ");
-  gets(infix);
-  infixtoprefix(infix,prefix);
-  reverse(prefix);
-  puts((prefix));
-}
-//--------------------------------------------------------
-void infixtoprefix(char infix[20],char prefix[20]) {
-  int i,j=0;
-  char symbol;
-  stack[++top]='#';
-  reverse(infix);
-  for (i=0;i<strlen(infix);i++) {
-    symbol=infix[i];
-    if (isOperator(symbol)==0) {
-      prefix[j]=symbol;
-      j++;
-    } else {
-      if (symbol==')') {
-        push(symbol);
-      } else if(symbol == '(') {
-        while (stack[top]!=')') {
-          prefix[j]=pop();
-          j++;
-        }
-        pop();
-      } else {
-        if (prcd(stack[top])<=prcd(symbol)) {
-          push(symbol);
-        } else {
-          while(prcd(stack[top])>=prcd(symbol)) {
-            prefix[j]=pop();
-            j++;
-          }
-          push(symbol);
-        }
-        //end for else
-      }
-    }
-    //end for else
-  }
-  //end for for
-  while (stack[top]!='#') {
-    prefix[j]=pop();
-    j++;
-  }
-  prefix[j]='\0';
-}
-////--------------------------------------------------------
-void reverse(char array[30])
-{ // for reverse of the given expression {
-  int i,j;
-  char temp[100];
-  for (i=strlen(array)-1,j=0;i+1!=0;--i,++j) {
-    temp[j]=array[i];
-  }
-  temp[j]='\0';
-  strcpy(array,temp);
-  return array;
-}
-//--------------------------------
-char pop() {
-  char a;
-  a=stack[top];
-  top--;
-  return a;
-}
-//----------------------------------
-void push(char symbol) {
-  top++;
-  stack[top]=symbol;
-}
-//------------------------------------------
-int prcd(symbol)
-{// returns the value that helps in the precedence {
-  switch(symbol) {
-    case '+':
-            case '-':
-            return 2;
-    break;
-    case '*':
-            case '/':
-            return 4;
-    break;
-    case '$':
-            case '^':
-            return 6;
-    break;
-    case '#':
-            case '(':
-            case ')':
-            return 1;
-    break;
-  }
-}
-//-------------------------------------------------
-int isOperator(char symbol) {
-  switch(symbol) {
-    case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '^':
-            case '$':
-            case '&':
-            case '(':
-            case ')':
-            return 1;
-    break;
-    default:
-            return 0;
-    // returns 0 if the symbol is other than given above
-  }
-}
+//Name Ankush kumar
+//Roll No 2020A1R157
+//Program to convert infix to postfix
+#include <stdio.h>  
+ #include <stdlib.h>  
+ #include <math.h>  
+ #include <string.h>  
+ #define MAX 100  
+ #define TAB '\t'  
+ #define BLANK ' '  
+ int top = -1;  
+ void push(long int symbol);  
+ long int pop();  
+ void infix_to_postfix();  
+ long int eval_post();  
+ int priority(char symbol);  
+ int isEmpty();  
+ int white_space(char symbol);  
+ char infix[MAX], postfix[MAX];  
+ long int stack[MAX];  
+ char *reverses(char str[]);  
+ char str_tmp[100];  
+ long long int count = 0;  
+ int main()  
+ {  
+   long int value;  
+   top = -1;  
+   printf("Enter the infix:"); //enter the whole expression here  
+   gets(infix);  
+   strcpy(infix, reverses(infix));//Here we reverse the expression  
+   infix_to_postfix(); //calling function  
+   printf("Prefix:%s\n", reverses(postfix));  
+   value = eval_post();  
+   printf("Value of expression: %ld\n", value);  
+ }  
+ void infix_to_postfix()  
+ {  
+   unsigned int i, p = 0;  
+   char next;  
+   char symbol;  
+   for (i = 0; i < strlen(infix); i++)  
+   {  
+     symbol = infix[i]; //assigning each value of infix expression to symbol  
+     if (!white_space(symbol))  
+     {  
+       switch (symbol)  
+       {  
+       case '(':  
+         push(symbol);  
+         break;  
+       case ')':  
+         while ((next = pop()) != '(')  
+         {  
+           postfix[p++] = next;  
+         }  
+         break;  
+       case '+':  
+       case '-':  
+       case '*':  
+       case '/':  
+       case '%':  
+       case '^':  
+         while ((!isEmpty()) && (priority(stack[top]) > priority(symbol)))  
+         {  
+           postfix[p++] = pop();  
+         }  
+         push(symbol);  
+         break;  
+       default:  
+         postfix[p++] = symbol;  
+       }  
+     }  
+   }  
+   while (!isEmpty()) //if some symbols remains left they are pop out here  
+   {  
+     postfix[p++] = pop();  
+   }  
+   postfix[p] = '\0'; //the expression in string format  
+ }  
+ char *reverses(char str[])//it reverse the string  
+ {  
+   int len = strlen(str);  
+   int j = 0;  
+   for (int i = len - 1; i >= 0; i--)  
+   {  
+     if (str[i] == '(')  
+     {  
+       str_tmp[j] = ')';  
+       j++;  
+     }  
+     else if (str[i] == ')')  
+     {  
+       str_tmp[j] = '(';  
+       j++;  
+     }  
+     else  
+     {  
+       str_tmp[j] = str[i];  
+       j++;  
+     }  
+   }  
+   str_tmp[j++] = '\0';  
+   return str_tmp;  
+ }  
+ int priority(char symbol)//it assign the priority to the operators  
+ {  
+   switch (symbol)  
+   {  
+   case '(':  
+     return 0;  
+     break;  
+   case '+':  
+   case '-':  
+     return 1;  
+     break;  
+   case '*':  
+   case '/':  
+   case '%':  
+     return 2;  
+     break;  
+   case '^':  
+     return 3;  
+     break;  
+   default:  
+     return 0;  
+   }  
+ }  
+ void push(long int symbol)  
+ {  
+   if (top > MAX)  
+   {  
+     printf("Stack overflow");  
+     return;  
+   }  
+   top = top + 1;  
+   stack[top] = symbol;  
+   count++;  
+ }  
+ long int pop()  
+ {  
+   if (isEmpty())  
+   {  
+     printf("Stack underflow\n");  
+     return 0;  
+   }  
+   return stack[top--];  
+ }  
+ int isEmpty()  
+ {  
+   if (top == -1)  
+   {  
+     return 1;  
+   }  
+   else  
+   {  
+     return 0;  
+   }  
+ }  
+ int white_space(char symbol)  
+ {  
+   if (symbol == BLANK || symbol == TAB)  
+   {  
+     return 1;  
+   }  
+   else  
+     return 0;  
+ }  
+ long int eval_post()//this function is for the evaluation of the expression  
+ {  
+   long int a, b, temp, result;  
+   unsigned int i;  
+   for (i = 0; i < strlen(postfix); i++)  
+   {  
+     if (postfix[i] <= '9' && postfix[i] >= '0')  
+     {  
+       push(postfix[i] - '0'); //conversion of string format to numeric for calcultion  
+     }  
+     else  
+     {  
+       a = pop();  
+       b = pop();  
+       switch (postfix[i])  
+       {  
+       case '+':  
+         temp = a + b;  
+         break;  
+       case '-':  
+         temp = a-b;  
+         break;  
+       case '*':  
+         temp = b * a;  
+         break;  
+       case '/':  
+         temp = a/b;  
+         break;  
+       case '%':  
+         temp = b% a;  
+         break;  
+       case '^':  
+         temp = pow(a, b);  
+         break;  
+       }  
+       push(temp);  
+     }  
+   }  
+   result = pop();  
+   return result;  
+ }
